4e53cb35bb2eb14ae274dc7d94c7f20d62801b8b
umesh kadam <umeshk149@gmail.com>
2021-02-08 15:42:44 +0530

    added textbox to test react-refesh

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Styles from './App.css';
 import Logo from './logo512.png';
 
 import Footer from './Footer/Footer';
 
 const App = () => {
+  const [state, setState] = useState('');
+
   return (
     <div>
       <div className={Styles.container}>
         <div>
           <img src={Logo} alt="Logo react" />
-
           {/* if image is from "public/images" folder */}
           {/* <img src="images/logo512.png" alt="Logo react" /> */}
         </div>
+        <form>
+          <input
+            type="text"
+            value={state}
+            placeholder="Start Typing....."
+            onChange={(e) => {
+              setState(e.target.value);
+            }}
+          />
+        </form>
         <div>
           <h1>React Refresh App</h1>
           <h3>
